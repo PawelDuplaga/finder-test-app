168625cf089c7e01f4460e40fb988c6f88f4556e
PawelDuplaga <pawelduplagait@gmail.com>
2023-10-04 22:57:40 +0200

    +expand function

--- src/components/CategoryGrid/CategoryGrid.tsx
+++ src/components/CategoryGrid/CategoryGrid.tsx
@@ -1,28 +1,42 @@
-import React from 'react'
+'use client'
+
+import React, { useMemo, useState } from 'react'
 import { categoriesTilesData } from '@/lib/categoriesTilesData'
 import CategoryTile from '../CategoryTile'
+import { IoIosArrowUp } from 'react-icons/io'
+import Link from 'next/link'
 
 
 
 const CategoryGrid = () => {
-  return (
-    <div className='max-w-[1120px]'>
-        <div className='flex flex-wrap justify-between'>
-            {categoriesTilesData.map((item) => (
-                <React.Fragment key={item.title}>
+
+    const [isExpanded, setIsExpanded] = useState(false);
+    const mappedCategoriesTitles = useMemo(() => {
+        return categoriesTilesData.map((item) => (
+            <React.Fragment key={item.title}>
                     <CategoryTile 
                         className='
-                        lg:w-[15%] lg:h-[110px] lg:mt-[1.25rem] 
-                        sm:w-[22%] sm:h-[110px] sm:mt-[1rem] 
-                        w-[48%] h-[110px] mt-[0.75rem] 
-                        rounded-xl border-2 justify-center items-center'
+                        lg:w-[15%] lg:h-[100px] lg:mt-[1.25rem] 
+                        sm:w-[22%] sm:h-[100px] sm:mt-[1rem] 
+                        w-[48%] h-[100px] mt-[1rem] 
+                        rounded-2xl border hover:border-blue-500 ustify-center items-center'
                         categoryTileProps={item}
                     />
-                </React.Fragment>
-            ))
-            
-            }
+            </React.Fragment>
+        ))
+    }, []);
+
+  return (
+    <div className='flex flex-col items-center'>
+        <div className={`max-w-[1120px] flex flex-col overflow-hidden transition ${isExpanded ? 'h-auto' : 'h-[360px] sm:h-[120px]'}`}>
+            <div className='flex flex-wrap justify-between'>
+                {mappedCategoriesTitles}       
+            </div>
         </div>
+        <button className='text-blue-500 mt-4 flex flex-row gap-2 items-center' onClick={() => setIsExpanded((current) => !current)}>
+            { isExpanded ? "Show less categories" : "Show more categories" }
+            <IoIosArrowUp className={`transition ${isExpanded ? 'rotate-180' : 'rotate-0'}`}/>
+        </button>
     </div>
   )
 }
--- src/components/CategoryTile/CategoryTile.tsx
+++ src/components/CategoryTile/CategoryTile.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { TCategoryTileProps } from '@/lib/categoriesTilesData'
-
+import Link from 'next/link'
 
 type TileProps = {
     categoryTileProps : TCategoryTileProps,
@@ -13,17 +13,19 @@ const CategoryTile = ({ categoryTileProps, className } : TileProps) => {
     const { icon, title, href } = categoryTileProps;
 
   return (
-    <div className={className}>
-        <div className='flex flex-col gap-2 justify-center items-center w-full h-full p-1'>
+    <Link className={className} href={href}>
+        <div className='flex flex-col gap-1 justify-center items-center w-full h-full p-1'>
             <Image 
                 src={icon}
                 alt={`${title} category icon`}
-                height={40}
-                width={40}
+                height={36}
+                width={36}
             />
-            <h3 className='text-center font-semibold text-blue-500'>{title}</h3>
+            <h3 className='text-center font-medium sm:text-[1rem] text-[0.85rem] 
+                         text-blue-500 leading-tight'
+            >{title}</h3>
         </div>
-    </div>
+    </Link>
   )
 }
 
